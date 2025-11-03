43269de2862459befa648ab967e1b7772d229f09
josevillarmontero-byte <jose.villar.montero@gmail.com>
2025-11-03 00:58:48 +0000

    Update storage_OCI.tf

--- other_format/storage_OCI.tf
+++ other_format/storage_OCI.tf
@@ -1,3 +1,13 @@
-resource "oci_storage_bucket" "example_oci" {
-    name = "example-oci-bucket"
-} 
+provider "oci" {
+        tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaaexampletenancy"
+        user_ocid        = "ocid1.user.oc1..aaaaaaaaexampleuser"
+        fingerprint      = "20:3b:97:13:example:fingerprint"
+        private_key_path = "~/.oci/oci_api_key.pem"
+        region           = "eu-frankfurt-1"
+        }
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = "mybucket"
+  key    = "app.zip"
+  source = "app.zip"
+}
