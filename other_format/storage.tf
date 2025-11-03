a2fd1b9cd7df7d9f5786bf07c857014519419410
josevillarmontero-byte <jose.villar.montero@gmail.com>
2025-11-03 00:57:02 +0000

    Update storage.tf

--- other_format/storage.tf
+++ other_format/storage.tf
@@ -1,3 +1,11 @@
-resource "oci_storage_bucket" "example" {
-    name = "example-bucket"
-} 
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AWS_ACCESS_KEY_ID"
+  secret_key = "AWS_SECRET_ACCESS_KEY"
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = "mybucket"
+  key    = "app.zip"
+  source = "app.zip"
+}
